751539f0d16cf73e3afb914c60d8717e6d2cb859
Bino <74612564+BinoGH@users.noreply.github.com>
2021-12-03 12:53:45 +0100

    Initial commit

---
+++ src/main/java/days/day4/star1/Star1.java
@@ -0,0 +1,11 @@
+package days.day4.star1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Star1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/main/java/days/day4/input.txt"));
+    }
+}
---
+++ src/main/java/days/day4/star2/Star2.java
@@ -0,0 +1,11 @@
+package days.day4.star2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Star2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("src/main/java/days/day4/input.txt"));
+    }
+}
